edb2e2199df23e917810ad505f4b04ca8e1d02bb
Keneth <k.ent@hotmail.com>
2024-01-18 00:00:39 -0600

    Abtract blocks of code into functions to simplify

--- deep_learning/ai_gym_tracker/src/app/counters.py
+++ deep_learning/ai_gym_tracker/src/app/counters.py
@@ -1,10 +1,10 @@
 """This module contains classes for counting exercises."""
+from typing import Dict
 from src.utils.utils import CounterUtils
 from src.app.models import Counter
 from src.app import mp_pose
 
 
-# Create curl counter class
 class CurlCounter(Counter, CounterUtils):
     """
     Class for counting bicep curls.
@@ -45,88 +45,54 @@ class CurlCounter(Counter, CounterUtils):
         self.right_state = 'start'
         self.left_state = 'start'
 
-    def get_important_landmarks(self):
+
+    def get_used_landmarks(self):
         """
         Returns the important landmarks for the exercise.
         """
-        return self.landmarks[:25]
+        return self.landmarks[:25] # From nose to right hip
+    
 
-    def make_calculations(self):
+    def generate_feedback(self, message: str, display_counters: bool = True) -> Dict[str, str]:
         """
-        Calculates the angle of the elbow and updates the current angle.
+        Formats the feedback to be displayed to the user.
         """
-        if self.check_starting_pose(self.get_important_landmarks()):
-            RIGHT_SHOULDER = [self.landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, self.landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            RIGHT_ELBOW = [self.landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, self.landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            RIGHT_WRIST = [self.landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, self.landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            self.current_right_angle = self.calculate_angle_3p(RIGHT_SHOULDER, RIGHT_ELBOW, RIGHT_WRIST)
-            LEFT_SHOULDER = [self.landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, self.landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            LEFT_ELBOW = [self.landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, self.landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-            LEFT_WRIST = [self.landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, self.landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-            self.current_left_angle = self.calculate_angle_3p(LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST)
-        else:
-            self.state = 'start'
-            self.right_state = 'start'
-            self.left_state = 'start'
-            self.output = {"counter": "", "message": "Please stand in the starting position", "sets": "", "reps": ""}
-            self.current_right_angle = self.resting_angle
-            self.current_left_angle = self.resting_angle
+        return {
+            "counter": [self.left_counter, self.right_counter] if display_counters else [0,0],
+            "sets": {"current": self.current_set, "total": self.num_sets},
+            "reps": self.reps_per_set,
+            "message": message
+        }
+    
 
-    def count(self):
+    def set_starting_pose(self, message: str = "Please stand in the starting position") -> None:
         """
-        Counts the number of bicep curls.
+        Resets the state of the counter.
         """
-        if self.state == 'start' and \
-            self.right_state == 'start' and \
-            self.left_state == 'start' and \
-            self.check_starting_pose(self.get_important_landmarks()
-        ):
-            self.state = 'counting'
-            self.right_state = 'down'
-            self.left_state = 'down'
+        self.state = 'start'
+        self.right_state = 'start'
+        self.left_state = 'start'
+        self.output = self.generate_feedback(message=message)
+        self.current_right_angle = self.resting_angle
+        self.current_left_angle = self.resting_angle
 
-        if self.current_right_angle > self.max_angle and self.right_state == 'up':
-            self.right_counter += 1
-            self.right_state = 'down'
-        if self.current_right_angle < self.min_angle and self.right_state == 'down':
-            self.right_state = 'up'
-            self.output = {
-                "counter": [self.left_counter, self.right_counter],
-                "sets": {"current": self.current_set, "total": self.num_sets},
-                "reps": self.reps_per_set,
-                "message": "Slowly curl down right"}
-        
-        if self.current_left_angle > self.max_angle and self.left_state == 'up':
-            self.left_counter += 1
-            self.left_state = 'down'
-        if self.current_left_angle < self.min_angle and self.left_state == 'down':
-            self.left_state = 'up'
-            self.output = {
-                "counter": [self.left_counter, self.right_counter],
-                "sets": {"current": self.current_set, "total": self.num_sets},
-                "reps": self.reps_per_set,
-                "message": "Slowly curl down left"}
 
-        if self.right_state == 'down' and self.left_state == 'down':
-            right_percentage = round(round(1 - (self.current_right_angle - self.min_angle) / (self.max_angle - self.min_angle), 2) * 100)
-            left_percentage = round(round(1 - (self.current_left_angle - self.min_angle) / (self.max_angle - self.min_angle), 2) * 100)
+    def arm_counting_cycle(self, side: str) -> None:
+        """
+        Counts the number of bicep curls for a single arm.
+        """
+        if self.__getattribute__(f"current_{side}_angle") > self.max_angle and self.__getattribute__(f"{side}_state") == 'up':
+            self.__setattr__(f"{side}_counter", self.__getattribute__(f"{side}_counter") + 1)
+            self.__setattr__(f"{side}_state", 'down')
+        if self.__getattribute__(f"current_{side}_angle") < self.min_angle and self.__getattribute__(f"{side}_state") == 'down':
+            self.__setattr__(f"{side}_state", 'up')
+            self.output = self.generate_feedback("Slowly curl down")
 
-            if right_percentage > 0:
-                self.output = {"counter": [self.left_counter, self.right_counter],
-                "sets": {"current": self.current_set, "total": self.num_sets},
-                "reps": self.reps_per_set,
-                "message": f"{max(right_percentage, 0)}% right curl up"}
-            elif left_percentage > 0:
-                self.output = {"counter": [self.left_counter, self.right_counter],
-                "sets": {"current": self.current_set, "total": self.num_sets},
-                "reps": self.reps_per_set,
-                "message": f"{max(left_percentage, 0)}% left curl up"}
-            else:
-                self.output = {"counter": [self.left_counter, self.right_counter],
-                "sets": {"current": self.current_set, "total": self.num_sets},
-                "reps": self.reps_per_set,
-                "message": "curl up one arm at a time"}
 
+    def check_completed_set(self) -> None:
+        """
+        Update and reset counters after a set is completed.
+        """
         self.reps_this_set = self.right_counter + self.left_counter
         if self.reps_this_set == self.reps_per_set * 2:
             self.current_set += 1
@@ -135,7 +101,66 @@ class CurlCounter(Counter, CounterUtils):
             self.left_counter = 0
             self.current_right_angle = self.resting_angle
             self.current_left_angle = self.resting_angle
+
+
+    def make_calculations(self) -> None:
+        """
+        Calculates the angle of the elbow and updates the current angle.
+        """
+        # Make sure the user is in the starting position
+        if not self.check_starting_pose(self.get_used_landmarks()):
+            self.set_starting_pose()
+            return
         
+        # Calculate the angle of the right elbow
+        RIGHT_SHOULDER = [self.landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, self.landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        RIGHT_ELBOW = [self.landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, self.landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        RIGHT_WRIST = [self.landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, self.landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        self.current_right_angle = self.calculate_angle_3p(RIGHT_SHOULDER, RIGHT_ELBOW, RIGHT_WRIST)
+
+        # Calculate the angle of the left elbow
+        LEFT_SHOULDER = [self.landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, self.landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+        LEFT_ELBOW = [self.landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, self.landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+        LEFT_WRIST = [self.landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, self.landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        self.current_left_angle = self.calculate_angle_3p(LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST)
+
+
+    def count(self) -> None:
+        """
+        Counts the number of bicep curls.
+        """
+        # Starting position -> counting position
+        if self.state == 'start' and self.check_starting_pose(self.get_used_landmarks()):
+            self.state = 'counting'
+            self.right_state = 'down'
+            self.left_state = 'down'
+
+        # Both arms counting cycles
+        self.arm_counting_cycle('right')
+        self.arm_counting_cycle('left')
+
+        # While both arms are down, display the percentage of the curl up
+        if self.right_state == 'down' and self.left_state == 'down':
+            right_percentage = round(round(1 - (self.current_right_angle - self.min_angle) / (self.max_angle - self.min_angle), 2) * 100)
+            left_percentage = round(round(1 - (self.current_left_angle - self.min_angle) / (self.max_angle - self.min_angle), 2) * 100)
+
+            # Check if both arms are up at the same time
+            if right_percentage > 0 and left_percentage > 0:
+                self.set_starting_pose(message="Please curl up one arm at a time!")
+
+            # Check if both arms are down at the same time
+            elif right_percentage <= 0 and left_percentage <= 0:
+                self.output = self.generate_feedback("curl up one arm at a time")
+
+            # Display the percentage of the curl up if only one arm is up
+            elif right_percentage > 0 or left_percentage > 0:
+                percentage = max(right_percentage if right_percentage > 0 else 0, left_percentage if left_percentage > 0 else 0)
+                self.output = self.generate_feedback(f"{percentage}% curl up")
+
+        # Check if the user is finished with the current set
+        self.check_completed_set()
+
+        # Change state to finished and set counter to max reps if the user is finished
         if self.is_finished():
             self.counter = self.reps_per_set * self.num_sets
             self.state = 'finished'
